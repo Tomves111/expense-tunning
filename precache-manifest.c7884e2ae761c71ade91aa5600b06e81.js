d8ff40afc26679ca660fcc5d60d43e615434e513
user5148540 <tomves111@gmail.com>
2020-06-07 10:28:47 +0200

    Updates

---
+++ precache-manifest.c7884e2ae761c71ade91aa5600b06e81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afd66e9aca27b67698c2f021a24b25d7",
+    "url": "/expense-tunning/index.html"
+  },
+  {
+    "revision": "a32a767a8668332475f3",
+    "url": "/expense-tunning/static/css/2.e2192fc4.chunk.css"
+  },
+  {
+    "revision": "fa4f9cd4715c517e571d",
+    "url": "/expense-tunning/static/css/main.d6bbec93.chunk.css"
+  },
+  {
+    "revision": "a32a767a8668332475f3",
+    "url": "/expense-tunning/static/js/2.8bd4e290.chunk.js"
+  },
+  {
+    "revision": "bfb205d13cec9044daf4e231c02cfc55",
+    "url": "/expense-tunning/static/js/2.8bd4e290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa4f9cd4715c517e571d",
+    "url": "/expense-tunning/static/js/main.7e63db60.chunk.js"
+  },
+  {
+    "revision": "ade1f4e4fea2720d426b",
+    "url": "/expense-tunning/static/js/runtime-main.7a19c86a.js"
+  }
+]);
